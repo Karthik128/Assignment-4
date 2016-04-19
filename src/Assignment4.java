cc6fa7d67da99e749ccb875c52fe9f67e0006e13
Karthik128 <dr.m.karthiik@gmail.com>
2016-04-19 18:14:25 +0530

    Assignment-4

---
+++ src/Assignment4.java
@@ -0,0 +1,20 @@
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Integer x=5;
+Integer y=10;
+
+Integer a= x+y*2;
+Integer b= x-y*2;
+Integer c= (x+y)*2;
+Integer d= y%x;
+
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+	}
+
+}
